8336dd9b723a2a5d47b10a863389eb72f3a2d81e
DadiB <38182988+DadiB134@users.noreply.github.com>
2019-01-09 18:55:24 +0100

    Update PresentMon-graph-creator.ps1

--- PresentMon-graph-creator.ps1
+++ PresentMon-graph-creator.ps1
@@ -6,6 +6,15 @@ param (
 if (!$(split-path $OutFile -IsAbsolute)) {
 	$OutFile=$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$OutFile"))
 }
+
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName System.Windows.Forms
+
+[xml]$ConfigFile=Get-Content "PresentMon-graph-creator-configuration.xml"
+$ImageBackground=$ConfigFile.configuration.AdvancedSettings.brush.ImageBackground.split(',')|%{iex $_}
+$GridlineBackground=$ConfigFile.configuration.AdvancedSettings.brush.GridlineBackground.split(',')|%{iex $_}
+$ColumnBackground=$ConfigFile.configuration.AdvancedSettings.brush.BarBackground.split(',')|%{iex $_}
+
 $csv=import-csv $InFile
 $raw=0
 $tot=0
@@ -18,31 +27,28 @@ $csv | foreach-object {
 	if ([double]$_.MsUntilDisplayed -lt $min) {$min=[double]$_.MsUntilDisplayed}
 	$raw++
 }
-Add-Type -AssemblyName System.Drawing
-Add-Type -AssemblyName System.Windows.Forms
-
 if ($Title) {
-$bmp=new-object System.Drawing.Bitmap 1000,350
+	$bmp=new-object System.Drawing.Bitmap 1000,350
 } else {
-$bmp=new-object System.Drawing.Bitmap 1000,250
+	$bmp=new-object System.Drawing.Bitmap 1000,250
 }
-$brushBg = [System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(0,0,0,0))
-$brushLn = [System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,127,127,127))
-$brushGp = [System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,75,125,75))
-$brushTx = [System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
-$fontTx = new-object System.Drawing.Font Arial,25
-$graphics = [System.Drawing.Graphics]::FromImage($bmp)
+$brushBg=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($ImageBackground[0],$ImageBackground[1],$ImageBackground[2],$ImageBackground[3]))
+$brushGl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($GridlineBackground[0],$GridlineBackground[1],$GridlineBackground[2],$GridlineBackground[3]))
+$brushGp=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($ColumnBackground[0],$ColumnBackground[1],$ColumnBackground[2],$ColumnBackground[3]))
+$brushTx=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
+$fontTx=new-object System.Drawing.Font Arial,25
+$graphics=[System.Drawing.Graphics]::FromImage($bmp)
 $graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height)
 if ($Title) {
-$brushTl = [System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
-$fontTl = new-object System.Drawing.Font Arial,25
-$graphics.FillRectangle($brushGp,250,0,500,50)
-$graphics.FillEllipse($brushGp,225,0,50,50)
-$graphics.FillEllipse($brushGp,725,0,50,50)
-$graphics.DrawString($Title,$fontTl,$brushTl,250+250-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).width/2),0+25-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).height/2))
-$offset=100
+	$brushTl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
+	$fontTl=new-object System.Drawing.Font Arial,25
+	$graphics.FillRectangle($brushGp,250,0,500,50)
+	$graphics.FillEllipse($brushGp,225,0,50,50)
+	$graphics.FillEllipse($brushGp,725,0,50,50)
+	$graphics.DrawString($Title,$fontTl,$brushTl,250+250-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).width/2),0+25-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).height/2))
+	$offset+=100
 }
-$graphics.FillRectangle($brushLn,172,$offset+10,3,230)
+$graphics.FillRectangle($brushGl,172,$offset+10,3,230)
 $graphics.FillRectangle($brushGp,175,$offset+25,($bmp.Width-300)/$max*($tot/$raw),50)
 $graphics.DrawString('Avarage: ',$fontTx,$brushTx,10,$offset+25+25-(([System.Windows.Forms.TextRenderer]::MeasureText($tot/$raw,$fontTx)).height/2))
 $graphics.DrawString([math]::Round(($tot/$raw),1),$fontTx,$brushTx,175+(($bmp.Width-300)/$max*($tot/$raw)),$offset+25+25-(([System.Windows.Forms.TextRenderer]::MeasureText($tot/$raw,$fontTx)).height/2))
