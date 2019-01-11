a47d79d3515aeaef18a1191dd0e41fffedaa2eae
DadiB <38182988+DadiB134@users.noreply.github.com>
2019-01-11 20:37:17 +0100

    Update PresentMon-graph-creator.ps1

--- PresentMon-graph-creator.ps1
+++ PresentMon-graph-creator.ps1
@@ -1,19 +1,22 @@
 param (
-	[string]$InFile=$(if(!$(Read-Host -Prompt 'Csv file name')){'graph.csv'}),
-	[string]$OutFile="graph.png",
+	[string]$InFile=$(Read-Host -Prompt 'Csv file name'),
+	[string]$OutFile,
 	[string]$Title=$(Read-Host -Prompt 'Title name')
 )
-if (!$(split-path $OutFile -IsAbsolute)) {
-	$OutFile=$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$OutFile"))
-}
+
+$ConfigFile=Import-LocalizedData -FileName PresentMon-graph-creator.psd1
+if (!$InFile) {$InFile=$ConfigFile.basic.InFile}
+if (!$OutFile) {$OutFile=$ConfigFile.basic.OutFile}
+if (!$(split-path $OutFile -IsAbsolute)) {$OutFile=$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$OutFile"))}
+$ImageBackground=$ConfigFile.advanced.ImageBackground.split(',')|%{iex $_}
+$GridlineBackground=$ConfigFile.advanced.GridlineBackground.split(',')|%{iex $_}
+$BarBackground=$ConfigFile.advanced.BarBackground.split(',')|%{iex $_}
+$LabelText=$ConfigFile.advanced.LabelText.split(',')|%{iex $_}
+$TitleText=$ConfigFile.advanced.TitleText.split(',')|%{iex $_}
+$TitleBackground=$ConfigFile.advanced.TitleBackground.split(',')|%{iex $_}
 
 Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName System.Windows.Forms
-
-[xml]$ConfigFile=Get-Content "PresentMon-graph-creator-configuration.xml"
-$ImageBackground=$ConfigFile.configuration.AdvancedSettings.brush.ImageBackground.split(',')|%{iex $_}
-$GridlineBackground=$ConfigFile.configuration.AdvancedSettings.brush.GridlineBackground.split(',')|%{iex $_}
-$ColumnBackground=$ConfigFile.configuration.AdvancedSettings.brush.BarBackground.split(',')|%{iex $_}
 
 $csv=import-csv $InFile
 $raw=0
@@ -34,28 +37,29 @@ if ($Title) {
 }
 $brushBg=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($ImageBackground[0],$ImageBackground[1],$ImageBackground[2],$ImageBackground[3]))
 $brushGl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($GridlineBackground[0],$GridlineBackground[1],$GridlineBackground[2],$GridlineBackground[3]))
-$brushGp=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($ColumnBackground[0],$ColumnBackground[1],$ColumnBackground[2],$ColumnBackground[3]))
-$brushTx=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
+$brushBb=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($BarBackground[0],$BarBackground[1],$BarBackground[2],$BarBackground[3]))
+$brushTx=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($LabelText[0],$LabelText[1],$LabelText[2],$LabelText[3]))
 $fontTx=new-object System.Drawing.Font Arial,25
 $graphics=[System.Drawing.Graphics]::FromImage($bmp)
 $graphics.FillRectangle($brushBg,0,0,$bmp.Width,$bmp.Height)
 if ($Title) {
-	$brushTl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb(255,0,0,0))
+	$brushTl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($TitleText[0],$TitleText[1],$TitleText[2],$TitleText[3]))
+	$brushTb=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($TitleBackground[0],$TitleBackground[1],$TitleBackground[2],$TitleBackground[3]))
 	$fontTl=new-object System.Drawing.Font Arial,25
-	$graphics.FillRectangle($brushGp,250,0,500,50)
-	$graphics.FillEllipse($brushGp,225,0,50,50)
-	$graphics.FillEllipse($brushGp,725,0,50,50)
+	$graphics.FillRectangle($brushTb,250,0,500,50)
+	$graphics.FillEllipse($brushTb,225,0,50,50)
+	$graphics.FillEllipse($brushTb,725,0,50,50)
 	$graphics.DrawString($Title,$fontTl,$brushTl,250+250-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).width/2),0+25-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).height/2))
 	$offset+=100
 }
 $graphics.FillRectangle($brushGl,172,$offset+10,3,230)
-$graphics.FillRectangle($brushGp,175,$offset+25,($bmp.Width-300)/$max*($tot/$raw),50)
+$graphics.FillRectangle($brushBb,175,$offset+25,($bmp.Width-300)/$max*($tot/$raw),50)
 $graphics.DrawString('Avarage: ',$fontTx,$brushTx,10,$offset+25+25-(([System.Windows.Forms.TextRenderer]::MeasureText($tot/$raw,$fontTx)).height/2))
 $graphics.DrawString([math]::Round(($tot/$raw),1),$fontTx,$brushTx,175+(($bmp.Width-300)/$max*($tot/$raw)),$offset+25+25-(([System.Windows.Forms.TextRenderer]::MeasureText($tot/$raw,$fontTx)).height/2))
-$graphics.FillRectangle($brushGp,175,$offset+100,($bmp.Width-300)/$max*$max,50)
+$graphics.FillRectangle($brushBb,175,$offset+100,($bmp.Width-300)/$max*$max,50)
 $graphics.DrawString('Maximum: ',$fontTx,$brushTx,10,$offset+100+25-(([System.Windows.Forms.TextRenderer]::MeasureText($max,$fontTx)).height/2))
 $graphics.DrawString([math]::Round($max,1),$fontTx,$brushTx,175+(($bmp.Width-300)/$max*$max),$offset+100+25-(([System.Windows.Forms.TextRenderer]::MeasureText($max,$fontTx)).height/2))
-$graphics.FillRectangle($brushGp,175,$offset+175,($bmp.Width-300)/$max*$min,50)
+$graphics.FillRectangle($brushBb,175,$offset+175,($bmp.Width-300)/$max*$min,50)
 $graphics.DrawString('Minimum: ',$fontTx,$brushTx,10,$offset+175+25-(([System.Windows.Forms.TextRenderer]::MeasureText($min,$fontTx)).height/2))
 $graphics.DrawString([math]::Round($min,1),$fontTx,$brushTx,175+(($bmp.Width-300)/$max*$min),$offset+175+25-(([System.Windows.Forms.TextRenderer]::MeasureText($min,$fontTx)).height/2))
 $graphics.Dispose()
