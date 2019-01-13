3005b741a03e8507ef228860364675b6141dd65c
DadiB <38182988+DadiB134@users.noreply.github.com>
2019-01-13 18:16:02 +0100

    Update PresentMon-graph-creator.ps1

--- PresentMon-graph-creator.ps1
+++ PresentMon-graph-creator.ps1
@@ -8,12 +8,14 @@ $ConfigFile=Import-LocalizedData -FileName PresentMon-graph-creator.psd1
 if (!$InFile) {$InFile=$ConfigFile.basic.InFile}
 if (!$OutFile) {$OutFile=$ConfigFile.basic.OutFile}
 if (!$(split-path $OutFile -IsAbsolute)) {$OutFile=$($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$OutFile"))}
+
 $ImageBackground=$ConfigFile.advanced.ImageBackground.split(',')|%{iex $_}
 $GridlineBackground=$ConfigFile.advanced.GridlineBackground.split(',')|%{iex $_}
 $BarBackground=$ConfigFile.advanced.BarBackground.split(',')|%{iex $_}
 $LabelText=$ConfigFile.advanced.LabelText.split(',')|%{iex $_}
 $TitleText=$ConfigFile.advanced.TitleText.split(',')|%{iex $_}
 $TitleBackground=$ConfigFile.advanced.TitleBackground.split(',')|%{iex $_}
+$TitleType=$ConfigFile.advanced.TitleType
 
 Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName System.Windows.Forms
@@ -46,9 +48,38 @@ if ($Title) {
 	$brushTl=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($TitleText[0],$TitleText[1],$TitleText[2],$TitleText[3]))
 	$brushTb=[System.Drawing.SolidBrush]::New([System.Drawing.Color]::FromArgb($TitleBackground[0],$TitleBackground[1],$TitleBackground[2],$TitleBackground[3]))
 	$fontTl=new-object System.Drawing.Font Arial,25
-	$graphics.FillRectangle($brushTb,250,0,500,50)
-	$graphics.FillEllipse($brushTb,225,0,50,50)
-	$graphics.FillEllipse($brushTb,725,0,50,50)
+	if ($TitleType -eq 0) {
+		$graphics.FillRectangle($brushTb,250,0,500,50)
+		$graphics.FillEllipse($brushTb,225,0,50,50)
+		$graphics.FillEllipse($brushTb,725,0,50,50)
+	} elseif ($TitleType -eq 1) {
+		$graphics.FillRectangle($brushTb,250,0,500,50)
+	} elseif ($TitleType -eq 2) {
+		$graphics.FillRectangle($brushTb,250,0,500,50)
+		$graphics.FillEllipse($brushTb,225,0,50,50)
+		$graphics.FillEllipse($brushTb,725,0,50,50)
+		$graphics.FillRectangle($brushTb,225,0,25,25)
+		$graphics.FillRectangle($brushTb,750,0,25,25)
+	} elseif ($TitleType -eq 3) {
+		$graphics.FillRectangle($brushTb,250,0,500,50)
+		$graphics.FillEllipse($brushTb,225,0,50,50)
+		$graphics.FillEllipse($brushTb,725,0,50,50)
+		$graphics.FillRectangle($brushTb,225,0,25,25)
+		$graphics.FillRectangle($brushTb,750,25,25,25)
+	} elseif ($TitleType -eq 4) {
+		$graphics.FillRectangle($brushTb,250,0,500,25)
+		$graphics.FillEllipse($brushTb,250,0,500,50)
+	} elseif ($TitleType -eq 5) {
+		$graphics.FillRectangle($brushTb,300,0,400,25)
+		$graphics.FillEllipse($brushTb,250,0,100,50)
+		$graphics.FillEllipse($brushTb,650,0,100,50)
+		$graphics.FillRectangle($brushTb,300,0,400,50)
+	} elseif ($TitleType -eq 6) {
+		$graphics.FillRectangle($brushTb,250,0,500,25)
+		$graphics.FillEllipse($brushTb,250,0,100,50)
+		$graphics.FillEllipse($brushTb,650,0,100,50)
+		$graphics.FillRectangle($brushTb,300,0,400,50)
+	}
 	$graphics.DrawString($Title,$fontTl,$brushTl,250+250-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).width/2),0+25-(([System.Windows.Forms.TextRenderer]::MeasureText($Title,$fontTl)).height/2))
 	$offset+=100
 }
@@ -63,9 +94,7 @@ $graphics.FillRectangle($brushBb,175,$offset+175,($bmp.Width-300)/$max*$min,50)
 $graphics.DrawString('Minimum: ',$fontTx,$brushTx,10,$offset+175+25-(([System.Windows.Forms.TextRenderer]::MeasureText($min,$fontTx)).height/2))
 $graphics.DrawString([math]::Round($min,1),$fontTx,$brushTx,175+(($bmp.Width-300)/$max*$min),$offset+175+25-(([System.Windows.Forms.TextRenderer]::MeasureText($min,$fontTx)).height/2))
 $graphics.Dispose()
-$bmp.Save($OutFile)
-Invoke-Item $OutFile
-echo "$($bar)"
+$bmp.Save("$OutFile")
 echo "Avarage: $($tot/$raw)"
 echo "Maximum: $($max)"
 echo "Minimum: $($min)"
